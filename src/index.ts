a710a899ffee4de6b8db6342ea676f12b73719af
Edgar Villegas <edgarinvillegas@hotmail.com>
2022-01-11 22:55:35 -0400

    feat: Add no-tagged-closures code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import noLiteral from "./rules/no-literal";
+import { rules } from './rules/no-tagged-closures';
 
 export = {
   rules: {
     "no-literal": noLiteral,
+    "no-tagged-closures": rules["no-tagged-closures"]
   },
 };
