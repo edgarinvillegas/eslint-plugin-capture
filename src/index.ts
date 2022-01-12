4a4337e2bae8d2e5e73c88f8e318cf554b0fe1b8
Edgar Villegas <edgarinvillegas@hotmail.com>
2022-01-11 23:21:18 -0400

    refactor: Remove tutorial files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import noLiteral from "./rules/no-literal";
 import noImplicitClosures from './rules/no-implicit-closures';
 
 export = {
   rules: {
-    "no-literal": noLiteral,
     "no-implicit-closures": noImplicitClosures,
   },
 };
